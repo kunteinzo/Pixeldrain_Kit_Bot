997e1ef7d7dd1ba4d797eafea1b7562dde8266f4
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2023-11-25 14:04:21 +0630

    update

---
+++ bot.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+source .env
+python3 main.py
+
--- main.py
+++ main.py
@@ -9,63 +9,66 @@ bot = telebot.TeleBot(os.environ.get('TOKEN'))
 
 print('Bot is running...')
 
-
-def udb():
-    with sqlite3.connect('database.db') as conn:
+    
+def db():
+    with sqlite3.connect("database.db") as conn:
         cursor = conn.cursor()
-        cursor.execute(
-            "create table if not exists User"
-            "("
-            "id integer,"
-            "username text,"
-            "first_name text,"
-            "last_name text,"
-            "full_name text"
-            ");"
-        )
-        cursor.execute(
-            "create table if not exists Key"
-            "("
-            "uid integer,"
-            "apikey text,"
-            "foreign key (uid) references User(id)"
-            ");"
+        cursor.execute(f"""
+            create table if not exists User
+            (
+            id integer,
+            username text,
+            full_name text
+            );""")
+        cursor.execute("""
+            create table if not exists Keys
+            (
+            id integer primary key autoincrement,
+            uid integer,
+            apikey text,
+            foreign key (uid) references User(id)
+            );"""
         )
         conn.commit()
         return conn
-
-
+        
+        
 def add_user(user: User):
-    with udb() as conn:
+    with db() as conn:
         cursor = conn.cursor()
         cursor.execute(
-            "insert into User (id,username,first_name,last_name,full_name) values (?,?,?,?,?);",
-            (user.id, user.username, user.first_name, user.last_name, user.full_name)
+            "insert into User (id,username,full_name) values (?,?,?);",
+            (user.id, user.username, user.full_name)
         )
         conn.commit()
+        
+        
+def mykey(user: User):
+    with db() as conn:
+        return conn.cursor().execute("select * from Keys where uid=?;", (user.id,)).fetchall()
 
 
 def is_not_exist(user: User):
-    with udb() as conn:
-        return conn.cursor().execute("select * from User where id=?", (user.id,)) is None
-
-
-def is_key_not_valid(key: str):
-    return re.match('^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$', key) is None
-
-
+    with db() as conn:
+        return conn.cursor().execute("select * from User where id=?", (user.id,)).fetchone() is None
+        
+        
 def add_apikey(user: User, key: str):
-    if is_key_not_valid(key):
-        return False
-    with udb() as conn:
+    with db() as conn:
+        if is_key_not_valid(key):
+            return False
         cursor = conn.cursor()
-        if cursor.execute('select * from Key where apikey=?;', (key,)).fetchone() is None:
+        if cursor.execute('select * from Keys where apikey=?;', (key,)).fetchone() is None:
             cursor.execute(
-                "insert into Key (uid, apikey) values (?,?);",
+                "insert into Keys (uid, apikey) values (?,?);",
                 (user.id, key)
             )
         conn.commit()
         return True
+
+
+def is_key_not_valid(key: str):
+    return re.match('^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$', key) is None
 
 
 @bot.message_handler(commands=['start'])
@@ -76,37 +79,24 @@ def start(message: Message):
     bot.send_message(message.chat.id, f"Hello *{user.full_name}*\.", 'MarkdownV2')
 
 
-@bot.message_handler(commands=['add_key'])
-def add_key(message: Message):
-    key = message.text[9:].strip()
-    if add_apikey(message.from_user, key):
-        bot.send_message(message.chat.id, "Your key is added successfully.")
-    else:
-        bot.send_message(message.chat.id, "Failed to add your key.")
-
-
-@bot.message_handler(commands=['my_key'])
-def my_key(message: Message):
-    with udb() as conn:
-        cursor = conn.cursor()
-        kk = cursor.execute(
-            "select * from Key where uid=?;",
-            (message.from_user.id,)
-        ).fetchall()
-        keys = '\n'.join([("`" + apikey.replace('-', '\-') + "`") for _, apikey in kk])
-        bot.send_message(message.chat.id, f"Your key are \n{keys}", 'MarkdownV2')
-
-
-@bot.message_handler(commands=['delete_key'])
-def delete_key(message: Message):
-    key = message.text[9:].strip()
+@bot.message_handler(commands=['addkey'])
+def addkey(message: Message):
+    key = message.text[8:].strip()
     if is_key_not_valid(key):
-        bot.send_message(message.chat.id, 'Api Key not valid.')
+        bot.send_message(message.chat.id, "Your Key is invalid.")
         return
-    with udb() as conn:
+    with db() as conn:
         cursor = conn.cursor()
-        cursor.execute("delete from Key where apikey=?", (key,))
-        conn.commit()
+        if cursor.execute("select * from Keys where apikey=?;", (key,)) is None:
+            cursor.execute("insert into Keys (uid, apikey) values (?,?);", (message.from_user.id, key))
+            conn.commit()
+        bot.send_message(message.chat.id, "Your Key have been added.")
+
+
+@bot.message_handler(commands=['mykey'])
+def my_key(message: Message):
+    keys = "\n".join(["`"+id+"` - `"+apikey.replace("-","\-")+"`" for id,_,apikey in mykey(message.from_user)])
+    bot.send_message(message.chat.id, f"Your keys are {keys}", 'MarkdownV2')
 
 
 bot.infinity_polling()
